b7bdb3a384fc03c936c13bd459a112671fa0b379
Thisal <thisal.18@itfac.mrt.ac.lk>
2021-08-24 09:00:34 +0530

    cart function done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,6 +14,7 @@ const reducer = combineReducers({
   cart: cartReducer,
 })
 
+//get Cart items from localStorage
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
